d64f3af9f2c528bb74fc30478628c8b3c6d43e55
Steve (private) Hindmarsh <stevehindmarsh@hotmail.co.uk>
2024-03-05 20:54:37 +0000

    Added Unit Tests Project

---
+++ Ncfe.CodeTest.UnitTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Ncfe.CodeTest.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            LearnerService learnerService = new LearnerService();
+            Learner learner = learnerService.GetLearner(1, true);
+            Assert.IsNotNull(learner);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            LearnerService learnerService = new LearnerService();
+            Learner learner = learnerService.GetLearner(1, false);
+            Assert.IsNotNull(learner);
+        }
+    }
+}
